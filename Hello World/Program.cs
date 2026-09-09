7a2de271a1da5b9d4558e7ae8ffba6954d2e5177
Chase Ries <chaseries77@gmail.com>
2026-09-09 14:57:16 -0400

    Add header comment and introduction lines

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Your Full Name
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 8, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Your Full Name.");
+Console.WriteLine("This is my first program at ETSU.");
